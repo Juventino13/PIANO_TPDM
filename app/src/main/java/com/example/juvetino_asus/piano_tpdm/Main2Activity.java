4729ec9c3a16da9e39c71940f67a75f4da21e460
Juventino13 <juventus139@hotmail.com>
2019-05-23 11:08:17 -0500

    feat: movimiento activitys
    Acercade...

--- app/src/main/java/com/example/juvetino_asus/piano_tpdm/Main2Activity.java
+++ app/src/main/java/com/example/juvetino_asus/piano_tpdm/Main2Activity.java
@@ -1,7 +1,10 @@
 package com.example.juvetino_asus.piano_tpdm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    backbutton();
+    }
+
+    private void backbutton() {
+            Button acercade= (Button) findViewById(R.id.infopiano);
+            acercade.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                   finish();
+                }
+            });
     }
 }
--- app/src/main/java/com/example/juvetino_asus/piano_tpdm/MainActivity.java
+++ app/src/main/java/com/example/juvetino_asus/piano_tpdm/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.juvetino_asus.piano_tpdm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        acercadetButton();
+        pianoButton();
+        informacioButton();
+        notasButton();
+
     }
+
+    private void notasButton() {
+        Button acercade= (Button) findViewById(R.id.infonotas);
+        acercade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( MainActivity.this,Main3Activity.class));
+            }
+        });
+    }
+
+    private void informacioButton() {
+        Button acercade= (Button) findViewById(R.id.infopiano);
+        acercade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( MainActivity.this,Main2Activity.class));
+            }
+        });
+    }
+
+
+    private void pianoButton() {
+        Button acercade= (Button) findViewById(R.id.piano);
+        acercade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( MainActivity.this,Main4Activity.class));
+            }
+        });
+    }
+
+
+
+    private void acercadetButton() {
+        Button acercade= (Button) findViewById(R.id.Acercade);
+        acercade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 startActivity(new Intent( MainActivity.this,Main5Activity.class));
+            }
+        });
+    }
+
 }
